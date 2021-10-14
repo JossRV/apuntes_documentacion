b9d43125552abee6df6dd7c647b1bff00ebe1c83
JossRV <jossrv74@gmail.com>
2021-10-14 00:31:12 -0500

    Agregando maquetado js

---
+++ Maquetados.js
@@ -0,0 +1,53 @@
+
+    // las etiquetas scrip se colocan en el doc html
+    // despues de la etiqueta html final para 
+    // su funcionamiento
+    // Impresion de dato
+    console.log("hola mundo");
+    // Crear variable
+    var nombre="Joss";
+    // imprimiendo nombre
+    console.log("El nombre es: "+nombre);
+    // let tambien se usa como variable, palabra reservada let
+    let apellido_paterno ='Velazquez';
+    // impresion con concatenacion
+    console.log("paterno es: "+apellido_paterno);
+    
+    let apellido_materno ='Nava';
+
+    console.log('materno es: '+apellido_materno);
+    edad = 34;
+
+    console.error(`La edad es ${edad}`);
+
+    /**
+     * TIP DE BIBLIA
+     * 
+     * USAR PUNTOS Y COMAS PARA CADA TERMINO DE LINEA
+     * APATRTE PARA NO DESACOSTUMBRARSE Y NO OLVIDAR ENTRE OTROS CODIGOS
+     * 
+     */
+
+    /**
+     *tipos de datos
+     */
+
+    //Cadenas de texto
+    //String
+    let cadena ='hola'
+    cadena = 'w'
+
+    //numerico
+    let numero = 34
+    numero = 90.67
+
+    //logico
+    let boleano = true
+    boleano = false
+
+    //null > nulo
+    let nulo = null
+    numero = null
+
+    //undefined > indefinido
+    let promedio
