fcda755603ec3d94fb7d9d4257b30d0c56105202
Danx <danidannniel398@gmail.com>
2022-04-23 21:19:53 -0500

    nuevo loader

--- pruebas_danx/loaders/loaders.php
+++ pruebas_danx/loaders/loaders.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loaders</title>
     <link rel="stylesheet" href="loaders.css">
-    
+
 </head>
+
 <body>
     <h2>Loader 1</h2>
     <div class="loader">
@@ -23,5 +25,9 @@
         <div class="loader3">
             <div class="loader3 loader-inner"></div>
         </div>
+    </div>
+    <h2>Loader 4</h2>
+    <div class="spinner"></div>
 </body>
+
 </html>
