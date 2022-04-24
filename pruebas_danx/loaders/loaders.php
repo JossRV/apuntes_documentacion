d66f6686b7b01a1247f677aa381a3ebaf8d83533
Danx <danidannniel398@gmail.com>
2022-04-23 21:14:02 -0500

    ejemplos de loaders y de textos, aun faltan los textos

---
+++ pruebas_danx/loaders/loaders.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loaders</title>
+    <link rel="stylesheet" href="loaders.css">
+    
+</head>
+<body>
+    <h2>Loader 1</h2>
+    <div class="loader">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+    </div>
+    <h2>Loader 2</h2>
+    <span class="loader2"></span>
+    <br><br><br><br><br>
+    <h2>Loader 3</h2>
+    <div class="loader-wrapper">
+        <div class="loader3">
+            <div class="loader3 loader-inner"></div>
+        </div>
+</body>
+</html>
