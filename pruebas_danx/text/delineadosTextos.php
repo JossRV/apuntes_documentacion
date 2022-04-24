c4062ce7bc522facfaeb97245b90275efcf3702f
Danx <danidannniel398@gmail.com>
2022-04-24 15:21:08 -0500

    ejemplos de animaciones en links
    ejemplos de delineados en textos

---
+++ pruebas_danx/text/delineadosTextos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Efectos de Textos</title>
+    <link rel="stylesheet" href="./textos.css">
+</head>
+<body>
+    <h1 class="seleccion">Delineado con color</h1>
+    <p class="text seleccion">Hola mundo, Texto delineado con Hover</p>
+    <h1 class="seleccion">Tipos de Underline en texto</h1>
+    <p class="text_delineado seleccion">Hola mundo</p>
+    <p class="text_delineado2 seleccion">Hola mundo</p>
+    <p class="text_delineado3 seleccion">Hola mundo</p>
+    <p class="text_delineado4 seleccion">Hola mundo</p>
+    <p class="text_delineado5 seleccion">Hola mundo</p>
+    <p class="text_delineado6 seleccion">Hola mundo</p>
+</body>
+</html>
---
+++ pruebas_danx/text/links_animation.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Links animation</title>
+    <link rel="stylesheet" href="./textos.css">
+</head>
+<body>
+    <a href="#" class="link">Hello</a>
+    <a href="#" class="link2">Hello World</a>
+    
+</body>
+</html>
