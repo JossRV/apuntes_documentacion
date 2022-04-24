2f97bdf766deb3be149260b2bd703ee0b7b39232
Danx <danidannniel398@gmail.com>
2022-04-23 21:32:05 -0500

    carpeta y ejemplo de iconos

---
+++ pruebas_danx/iconos/iconos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Floating element animation</title>
+    <!-- Font icons pulled from remix icon CDN (Content delivery network) -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <!-- Link to your stylesheet -->
+    <link rel="stylesheet" href="./iconos.css">
+</head>
+<body>
+    <div class="icons-wrapper">
+        <i class="ri-instagram-line icon"></i>
+        <i class="ri-facebook-circle-line icon"></i>
+        <i class="ri-whatsapp-line icon"></i>
+    </div>
+
+</body>
+</html>
