1b7bf7a0a0514b35326211021ed6cbfc8cf44012
JossRV <jossrv74@gmail.com>
2022-05-02 00:40:17 -0500

    footer con olas

---
+++ PracticasJoss/footer/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <footer>
+        <div class="waves">
+            <div class="wave" id="wave1"></div>
+            <div class="wave" id="wave2"></div>
+            <div class="wave" id="wave3"></div>
+            <div class="wave" id="wave4"></div>
+        </div>
+        <ul class="social">
+            <li>
+                <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+            </li>
+            <li>
+                <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+            </li>
+            <li>
+                <a href="#"><ion-icon name="logo-github"></ion-icon></a>
+            </li>
+        </ul>
+        <ul class="menu">
+            <li>
+                <a href="#">Inicio</a>
+            </li>
+            <li>
+                <a href="#">Nosotros/contactos</a>
+            </li>
+            <li>
+                <a href="#">Servicios</a>
+            </li>
+            <li>
+                <a href="#">Team</a>
+            </li>
+        </ul>
+        <p>Todos los derechos reservados hecho en 2022 xd</p>
+    </footer>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
