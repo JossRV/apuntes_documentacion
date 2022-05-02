2805b2c32ed0ed2eee256aa9998ade1d2b77bc49
JossRV <jossrv74@gmail.com>
2022-05-01 23:15:45 -0500

    tarjeta de perfil

---
+++ PracticasJoss/tarjetaPerfil/tarjetadePerfil.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarjeta con datos</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="tarjeta">
+        <div class="imagen">
+            <img src="myImg.jpeg" alt="">
+        </div>
+        <div class="contenido">
+            <div class="detalles">
+                <h2>
+                    Jose Alberto 
+                    <br>
+                    <span>Desarrollador Web Junior</span>
+                </h2>
+                <div class="datos">
+                    <h3>vengo <br><span>ITMA II</span></h3>
+                    <h3>vengo <br><span>ITMA II</span></h3>
+                    <h3>vengo <br><span>ITMA II</span></h3>
+                </div>
+                <div class="paginas">
+                    <a href="https://github.com/JossRV">Mi Git Hub</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
