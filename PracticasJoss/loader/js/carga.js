9b1e365d178fb5d5aa8ac09f3c240f1641a4bc38
JossRV <jossrv74@gmail.com>
2022-12-08 22:22:00 -0600

    updating...

--- PracticasJoss/loader/js/carga.js
+++
@@ -1,6 +0,0 @@
-
-window.onload = function(){
-    var contenedor = document.getElementById('container_carga');
-    contenedor.style.visibility = 'hidden';
-    contenedor.style.opacity = 0;
-}
--- PracticasJoss/menuFlotante/menuFlotante.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function(){
-    const menu_desplegable = document.querySelector('.menu_desplegable');
-    const navegacion = document.querySelector('.navegacion');
-
-    menu_desplegable.onclick = function(){
-        navegacion.classList.toggle('active');
-    }
-// });
